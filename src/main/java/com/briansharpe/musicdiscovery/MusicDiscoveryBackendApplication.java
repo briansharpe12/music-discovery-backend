133b1aa4e3e14ba3d2443803ef9e33b4cd691699
Brian Sharpe <briansharpe12@outlook.com>
2026-08-24 16:57:36 -0400

    chore: initialize Spring Boot backend and database connection

---
+++ src/main/java/com/briansharpe/musicdiscovery/MusicDiscoveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.briansharpe.musicdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDiscoveryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicDiscoveryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/briansharpe/musicdiscovery/MusicDiscoveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briansharpe.musicdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDiscoveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
